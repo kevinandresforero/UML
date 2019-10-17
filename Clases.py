0988fee9a7bcc96cf76aae02f03ce9a5ab4fb294
Kevin Andres Forero Guaitero <42241664+kevinandresforero@users.noreply.github.com>
2019-10-16 19:59:25 -0500

    Add files via upload

--- Clases.py
+++ Clases.py
@@ -17,10 +17,10 @@ def main():
         def crearClase(self):
 
             self.__columna = self.__columna + 1
-            print(str(self.__columna))
+            #print(str(self.__columna))
 
             self.__fila = self.__fila + 1
-            print(str(self.__fila))
+            #print(str(self.__fila))
             self.__atri = []
 
             auxNN=StringVar()
@@ -32,43 +32,40 @@ def main():
 
             self.__metodos = Text(marco, width = 20, height=10, bg="black" , fg="white")
             self.__metodos.grid(row=2,column=1)
-            
-            #****************************************#
 
             def crear():
-                nuevaven = tk.Toplevel(marco)
-                nuevaven.title('Clases')
-                nuevaven.geometry('300x300')
-                nuevaven.configure(bg="black")
-                
-
-                boton.config(state='disable')
-
-                '''def cierra_nueva():
-                    nueva_ventana.destroy()
-
-                    boton.config(state='normal')
-                cerrar = tk.Button(nueva_ventana,
-                    text='Cerrar nueva ventana',
-                    command=cierra_nueva)
-                cerrar.pack()
-
-                nueva_ventana.protocol("WM_DELETE_WINDOW", cerrar)'''
-
-            boton = tk.Button(marco, text="Mostrar clase", bg="black", fg="white", command=crear)
-            boton.grid(row=3, column=2, padx=5)
-            #boton.pack()
-
-            #****************************************#
-            def guardarDatos():
+                ventana = Tk()
+                lienzo = Canvas(ventana,
+                width=300, height=300,bg="#9AE5D3")
+                ventana.title("Clase")
+                lienzo.grid(row=0,column=100)
+                lienzo.create_rectangle(
+                5,5,295,295, fill="#2D6457", outline="RED",width=1)
+                lienzo.create_line(5, 50, 295, 50, width=1, fill='RED')
+                lienzo.create_line(5, 180, 295, 180, width=1, fill='RED')
 
                 most =auxNN.get()
-                print(most)
+                lienzo.create_text(50,30, text=str(most))
 
                 x=self.__atributos.get(1.0, END)
                 r = re.split("/n", x)
                 for i in r:
                     print(i)
+                lienzo.create_text(50,70, text=str(x))
+
+                ventana.mainloop()
+            #crear()
+
+            boton = tk.Button(marco, text="Mostrar clase", bg="black", fg="white", command=crear)
+            boton.grid(row=3, column=2, padx=5)
+            #boton.pack()
+
+            def guardarDatos():
+
+                
+                #print(most)
+
+
 
                 a=self.__metodos.get(1.0, END)
                 b = re.split("/n", a)
